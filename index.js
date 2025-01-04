d59ffdc04a0624a5bb2d202e125c1e64e92883d1
medsonmoombe <medsonmoombe21@gmail.com>
2025-01-04 19:26:57 +0200

    update cors

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // for testing
+    origin:'*',
     credentials:true
 }
 
