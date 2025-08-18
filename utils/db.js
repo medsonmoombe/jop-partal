844c6735bc76cf77beceb62ad4213d6afe93f4ac
medsonmoombe <medsonmoombe21@gmail.com>
2025-08-18 06:47:12 +0200

    updates

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            ssl: true,
+            tlsAllowInvalidCertificates: false,
+            tlsAllowInvalidHostnames: false
+        });
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
