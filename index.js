4ce38d0d76c3b6fdf5c69e47a736e550674afa74
medsonmoombe <medsonmoombe21@gmail.com>
2026-04-27 23:41:00 +0200

    Add

--- index.js
+++ index.js
@@ -22,8 +22,24 @@ app.use(helmet());
 app.use(mongoSanitize());
 
 // CORS configuration
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://jop-partal.onrender.com',
+    process.env.CLIENT_URL
+].filter(Boolean);
+
 const corsOptions = {
-    origin: '*',
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
