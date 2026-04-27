e65a713f9760f0b73c9ded300a793cb25284b86e
medsonmoombe <medsonmoombe21@gmail.com>
2026-04-27 23:41:27 +0200

    Add cors updats

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Body parser middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
